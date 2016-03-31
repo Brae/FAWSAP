d5f30b1685b1f16f3525bb489e544c100b446de6
Andy Barlow <itsSticky@Andys-MacBook-Air.local>
2016-03-31 09:50:12 +0100

    fixed xvwa submodule issue

---
+++ xvwa/config.php
@@ -0,0 +1,11 @@
+<?php
+$XVWA_WEBROOT = '';
+$host = "127.0.0.1";
+$dbname = 'xvwa';
+$user = 'root';
+$pass = 'letmein12';
+$conn = mysql_connect($host,$user,$pass);
+$conn = mysql_select_db($dbname);
+$conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
