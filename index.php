95944aaea00cb0c43f003324fefa6733b773cda8
GeneralBrae <pedrocowman@gmail.com>
2016-04-25 22:53:14 +0100

    Some changes

--- index.php
+++ index.php
@@ -455,13 +455,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <div class="box-header">
                   <!-- tools box -->
                   <div class="pull-right box-tools">
-                    <button class="btn btn-primary btn-sm daterange pull-right" data-toggle="tooltip" title="Date range"><i class="fa fa-calendar"></i></button>
                     <button class="btn btn-primary btn-sm pull-right" data-widget="collapse" data-toggle="tooltip" title="Collapse" style="margin-right: 5px;"><i class="fa fa-minus"></i></button>
                   </div><!-- /. tools -->
 
                   <i class="fa fa-map-marker"></i>
                   <h3 class="box-title">
-                    Visitors Online
+                    phD Project Link
                   </h3>
                 </div>
                 <div class="box-body">
